005baa71d83408217cf48e85da8ffb1e73caf436
Aman <aman16singh2001@gmail.com>
2023-06-18 11:12:28 +0530

    setting up react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <div className="app">
     Hello
     </div>
+    </Router>
   );
 }
 
